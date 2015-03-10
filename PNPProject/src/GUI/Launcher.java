ceac8a51bdafcf1cb0cd622001dd1bdff96d0179
arriggon <arriggon@gmail.com>
2015-03-10 14:57:22 +0100

    +changed organization

---
+++ PNPProject/src/GUI/Launcher.java
@@ -0,0 +1,15 @@
+package GUI;
+
+import javax.swing.*;
+
+/**
+ * Created by Arriggon on 3/10/2015.
+ */
+public class Launcher {
+    public class Layer extends JPanel {
+
+    }
+    public static void main(String[] args){
+
+    }
+}
--- PNPProject/src/Launcher.java
+++
@@ -1,6 +0,0 @@
-/**
- * Created by Arriggon on 3/10/2015.
- */
-public class Launcher {
-    
-}
