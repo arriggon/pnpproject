2bdaa9e15e3a32e242237216540ca220128e9ade
arriggon <arriggon@gmail.com>
2015-04-10 19:55:49 +0200

    ~

---
+++ PNPProject/src/GUI/Launcher.java
@@ -0,0 +1,17 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import java.lang.Exception;
+import java.lang.Override;
+
+public class Launcher extends Application{
+    public static void main(String[] args){
+        launch(args);
+    }
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
