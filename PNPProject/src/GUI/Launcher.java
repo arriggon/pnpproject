cdd39f8c07065986541f8b22ce9f94adad2f7d8f
arriggon <arriggon@gmail.com>
2015-04-21 13:14:58 +0200

    Further Refinements Of GUI classes

--- PNPProject/src/GUI/Launcher.java
+++ PNPProject/src/GUI/Launcher.java
@@ -1,12 +1,18 @@
 package GUI;
 
 import javafx.application.Application;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.lang.Exception;
 import java.lang.Override;
 
 public class Launcher extends Application{
+    //Elements
+    private Button join_btn;
+
     public static void main(String[] args){
         launch(args);
     }
@@ -14,6 +20,14 @@ public class Launcher extends Application{
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        
+        primaryStage.setWidth(200);
+        primaryStage.setHeight(500);
+        BorderPane main = new BorderPane();
+        VBox menu = new VBox(10);
+
+
+
+
+        primaryStage.show();
     }
 }
---
+++ PNPProject/src/GUI/OptionPane.java
@@ -0,0 +1,8 @@
+package GUI;
+
+/**
+ * Created by Arriggon on 4/21/2015.
+ */
+public class OptionPane {
+
+}
