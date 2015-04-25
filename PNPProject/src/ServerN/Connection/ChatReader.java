12ad36749def95456c53a3b5208f168bd414116f
Mcgamer01 <mcgamer0070@gmail.com>
2015-04-25 09:21:53 +0200

    Started Implementing chat functionality

--- PNPProject/src/ServerN/Connection/ChatReader.java
+++ PNPProject/src/ServerN/Connection/ChatReader.java
@@ -28,4 +28,6 @@ public class ChatReader implements Callable<Object> {
         return o;
 
     }
+
+
 }
--- PNPProject/src/ServerN/Connection/ChatReaderManager.java
+++ PNPProject/src/ServerN/Connection/ChatReaderManager.java
@@ -1,28 +1,33 @@
 package ServerN.Connection;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
 
 /**
  * Created by Alexander on 22.04.2015.
  */
-public class ChatReaderManager implements Runnable{
+public class ChatReaderManager{
 
-    private ArrayList<Future> chatters;
+    private HashMap<ChatReader, Future<Object>> chatters;
     private ExecutorService t;
 
     public ChatReaderManager(ExecutorService t) {
-        this.chatters = new ArrayList<Future>();
+        this.chatters = new HashMap<ChatReader, Future<Object>>();
         this.t = t;
     }
 
     public void addChatter(ChatReader reader) {
-
+        Future<Object> f = t.submit(reader);
+        chatters.put(reader, f);
     }
 
-    @Override
-    public void run() {
-
+    public void removeChatter(ChatReader r) {
+        Future<Object> future = chatters.get(r);
+        chatters.remove(r);
+        future.cancel(true);
     }
+
+
 }
