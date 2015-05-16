bea6678168bf8de03ca21c395ebe4ed3bb4145af
Mcgamer01 <mcgamer0070@gmail.com>
2015-05-16 16:33:00 +0200

    Introducting DisconnectionNotification Object

---
+++ PNPProject/src/Model/Request/DisconnectNotification.java
@@ -0,0 +1,11 @@
+package Model.Request;
+
+import Model.DataOverNetwork;
+
+import java.io.Serializable;
+
+/**
+ * Created by RAIDER on 16.05.2015.
+ */
+public class DisconnectNotification implements Serializable, DataOverNetwork {
+}
